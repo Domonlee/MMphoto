78fee709cd2e1e54fac992ea00322b665780a7eb
Domon <viplizhao@gmail.com>
2017-08-13 18:10:39 +0800

    modify AlbumData

--- app/src/main/java/domon/cn/mmphoto/data/AlbumData.java
+++ app/src/main/java/domon/cn/mmphoto/data/AlbumData.java
@@ -14,14 +14,14 @@ public class AlbumData {
      * recommend : [{"ID":12,"AtlasID":2326,"AtlasTitle":"ROSI写真No.2073 黑色连体开档皮衣翘臀","AtlasImg":"http://uuu.shafa5.com/ros/2326/2073.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/10/2326.htm","AtlasCost":20},{"ID":13,"AtlasID":2325,"AtlasTitle":"ROSI写真No.2072 透视黑纱睡衣金色短裤","AtlasImg":"http://uuu.shafa5.com/ros/2325/2072.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/09/2325.htm","AtlasCost":20},{"ID":14,"AtlasID":2324,"AtlasTitle":"ROSI写真No.2071 运动装一字马劈腿展示","AtlasImg":"http://uuu.shafa5.com/ros/2324/2071.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/08/2324.htm","AtlasCost":20},{"ID":15,"AtlasID":2323,"AtlasTitle":"ROSI写真No.2070 黑色情趣睡衣吊带丝袜性感写真","AtlasImg":"http://uuu.shafa5.com/ros/2323/2070.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/07/2323.htm","AtlasCost":20},{"ID":16,"AtlasID":2322,"AtlasTitle":"ROSI写真No.2069 巨乳红色包臀短裙性感撩人","AtlasImg":"http://uuu.shafa5.com/ros/2322/2069.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/06/2322.htm","AtlasCost":20},{"ID":17,"AtlasID":2321,"AtlasTitle":"ROSI写真No.2068 红色背背短裙抹胸花内内","AtlasImg":"http://uuu.shafa5.com/ros/2321/2068.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/05/2321.htm","AtlasCost":20},{"ID":18,"AtlasID":2320,"AtlasTitle":"ROSI写真No.2067 衬衣黑丝倒挂金钩表演","AtlasImg":"http://uuu.shafa5.com/ros/2320/2067.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/04/2320.htm","AtlasCost":20},{"ID":19,"AtlasID":2319,"AtlasTitle":"ROSI写真No.2066 马尾萝莉内衣肉丝写真","AtlasImg":"http://uuu.shafa5.com/ros/2319/2066.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/03/2319.htm","AtlasCost":20},{"ID":20,"AtlasID":2318,"AtlasTitle":"ROSI写真No.2065 黑色蕾丝背心紫色内内","AtlasImg":"http://uuu.shafa5.com/ros/2318/2065.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/02/2318.htm","AtlasCost":20},{"ID":21,"AtlasID":2317,"AtlasTitle":"ROSI写真No.2064 半裸红色丝袜圆润翘臀秀","AtlasImg":"http://uuu.shafa5.com/ros/2317/2064.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/01/2317.htm","AtlasCost":20}]
      */
 
-    private AtlasEntity atlas;
+    private PhotoData atlas;
     private List<PhotoData> recommend;
 
-    public AtlasEntity getAtlas() {
+    public PhotoData getAtlas() {
         return atlas;
     }
 
-    public void setAtlas(AtlasEntity atlas) {
+    public void setAtlas(PhotoData atlas) {
         this.atlas = atlas;
     }
 
@@ -31,71 +31,5 @@ public class AlbumData {
 
     public void setRecommend(List<PhotoData> recommend) {
         this.recommend = recommend;
-    }
-
-    public static class AtlasEntity {
-        /**
-         * ID : 38
-         * AtlasID : 2300
-         * AtlasTitle : ROSI写真No.2047 浴室真空肉丝美臀玉腿写真
-         * AtlasImg : http://uuu.shafa5.com/ros/2300/2047.jpg
-         * AtlasToUrl : http://www.rosmm.com/rosimm/2017/07/15/2300.htm
-         * AtlasCost : 20
-         */
-
-        private int ID;
-        private int AtlasID;
-        private String AtlasTitle;
-        private String AtlasImg;
-        private String AtlasToUrl;
-        private int AtlasCost;
-
-        public int getID() {
-            return ID;
-        }
-
-        public void setID(int ID) {
-            this.ID = ID;
-        }
-
-        public int getAtlasID() {
-            return AtlasID;
-        }
-
-        public void setAtlasID(int AtlasID) {
-            this.AtlasID = AtlasID;
-        }
-
-        public String getAtlasTitle() {
-            return AtlasTitle;
-        }
-
-        public void setAtlasTitle(String AtlasTitle) {
-            this.AtlasTitle = AtlasTitle;
-        }
-
-        public String getAtlasImg() {
-            return AtlasImg;
-        }
-
-        public void setAtlasImg(String AtlasImg) {
-            this.AtlasImg = AtlasImg;
-        }
-
-        public String getAtlasToUrl() {
-            return AtlasToUrl;
-        }
-
-        public void setAtlasToUrl(String AtlasToUrl) {
-            this.AtlasToUrl = AtlasToUrl;
-        }
-
-        public int getAtlasCost() {
-            return AtlasCost;
-        }
-
-        public void setAtlasCost(int AtlasCost) {
-            this.AtlasCost = AtlasCost;
-        }
     }
 }
---
+++ app/src/main/java/domon/cn/mmphoto/data/CategoryDetailData.java
@@ -0,0 +1,47 @@
+package domon.cn.mmphoto.data;
+
+import java.util.List;
+
+/**
+ * Created by Domon on 2017/8/13.
+ */
+
+public class CategoryDetailData {
+
+    private List<AtlasBean> atlas;
+
+    public List<AtlasBean> getAtlas() {
+        return atlas;
+    }
+
+    public void setAtlas(List<AtlasBean> atlas) {
+        this.atlas = atlas;
+    }
+
+    public static class AtlasBean {
+        /**
+         * type : 1
+         * atlasList : [{"ID":12,"AtlasID":"2326","AtlasTitle":"ROSI写真No.2073 黑色连体开档皮衣翘臀","AtlasImg":"http://uuu.shafa5.com/ros/2326/2073.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/10/2326.htm","AtlasCost":20,"AtlasType":1},{"ID":13,"AtlasID":"2325","AtlasTitle":"ROSI写真No.2072 透视黑纱睡衣金色短裤","AtlasImg":"http://uuu.shafa5.com/ros/2325/2072.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/09/2325.htm","AtlasCost":20,"AtlasType":1},{"ID":14,"AtlasID":"2324","AtlasTitle":"ROSI写真No.2071 运动装一字马劈腿展示","AtlasImg":"http://uuu.shafa5.com/ros/2324/2071.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/08/2324.htm","AtlasCost":20,"AtlasType":1},{"ID":15,"AtlasID":"2323","AtlasTitle":"ROSI写真No.2070 黑色情趣睡衣吊带丝袜性感写真","AtlasImg":"http://uuu.shafa5.com/ros/2323/2070.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/07/2323.htm","AtlasCost":20,"AtlasType":1},{"ID":16,"AtlasID":"2322","AtlasTitle":"ROSI写真No.2069 巨乳红色包臀短裙性感撩人","AtlasImg":"http://uuu.shafa5.com/ros/2322/2069.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/06/2322.htm","AtlasCost":20,"AtlasType":1},{"ID":17,"AtlasID":"2321","AtlasTitle":"ROSI写真No.2068 红色背背短裙抹胸花内内","AtlasImg":"http://uuu.shafa5.com/ros/2321/2068.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/05/2321.htm","AtlasCost":20,"AtlasType":1},{"ID":18,"AtlasID":"2320","AtlasTitle":"ROSI写真No.2067 衬衣黑丝倒挂金钩表演","AtlasImg":"http://uuu.shafa5.com/ros/2320/2067.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/04/2320.htm","AtlasCost":20,"AtlasType":1},{"ID":19,"AtlasID":"2319","AtlasTitle":"ROSI写真No.2066 马尾萝莉内衣肉丝写真","AtlasImg":"http://uuu.shafa5.com/ros/2319/2066.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/03/2319.htm","AtlasCost":20,"AtlasType":1},{"ID":20,"AtlasID":"2318","AtlasTitle":"ROSI写真No.2065 黑色蕾丝背心紫色内内","AtlasImg":"http://uuu.shafa5.com/ros/2318/2065.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/02/2318.htm","AtlasCost":20,"AtlasType":1},{"ID":21,"AtlasID":"2317","AtlasTitle":"ROSI写真No.2064 半裸红色丝袜圆润翘臀秀","AtlasImg":"http://uuu.shafa5.com/ros/2317/2064.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/08/01/2317.htm","AtlasCost":20,"AtlasType":1},{"ID":22,"AtlasID":"2316","AtlasTitle":"ROSI写真No.2063 沙发上的绿丝高难度一柱擎天","AtlasImg":"http://uuu.shafa5.com/ros/2316/2063.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/31/2316.htm","AtlasCost":20,"AtlasType":1},{"ID":23,"AtlasID":"2315","AtlasTitle":"ROSI写真No.2062 内衣床照梦幻特写","AtlasImg":"http://uuu.shafa5.com/ros/2315/2062.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/30/2315.htm","AtlasCost":20,"AtlasType":1},{"ID":24,"AtlasID":"2314","AtlasTitle":"ROSI写真No.2061 黑T恤超短裤显T裤丰满肥臀","AtlasImg":"http://uuu.shafa5.com/ros/2314/2061.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/29/2314.htm","AtlasCost":20,"AtlasType":1},{"ID":25,"AtlasID":"2313","AtlasTitle":"ROSI写真No.2060 黑丝网袜情趣透视装俯视特写","AtlasImg":"http://uuu.shafa5.com/ros/2313/2060.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/28/2313.htm","AtlasCost":20,"AtlasType":1},{"ID":26,"AtlasID":"2312","AtlasTitle":"ROSI写真No.2059 新潮女仆装椅子上的翘臀表演","AtlasImg":"http://uuu.shafa5.com/ros/2312/2059.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/27/2312.htm","AtlasCost":20,"AtlasType":1},{"ID":27,"AtlasID":"2311","AtlasTitle":"ROSI写真No.2058 难得一见的巨乳抹胸T裤美女","AtlasImg":"http://uuu.shafa5.com/ros/2311/2058.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/26/2311.htm","AtlasCost":20,"AtlasType":1},{"ID":28,"AtlasID":"2310","AtlasTitle":"ROSI写真No.2057 内衣蓝色包臀短裙肉丝美腿","AtlasImg":"http://uuu.shafa5.com/ros/2310/2057.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/25/2310.htm","AtlasCost":20,"AtlasType":1},{"ID":29,"AtlasID":"2309","AtlasTitle":"ROSI写真No.2056 黑色连体紧身衣蛙式写真","AtlasImg":"http://uuu.shafa5.com/ros/2309/2056.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/24/2309.htm","AtlasCost":20,"AtlasType":1},{"ID":30,"AtlasID":"2308","AtlasTitle":"ROSI写真No.2055 性感水手内衣修长美腿","AtlasImg":"http://uuu.shafa5.com/ros/2308/2055.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/23/2308.htm","AtlasCost":20,"AtlasType":1},{"ID":31,"AtlasID":"2307","AtlasTitle":"ROSI写真No.2054 紧身运动衣肉丝翘臀美腿","AtlasImg":"http://uuu.shafa5.com/ros/2307/2054.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/22/2307.htm","AtlasCost":20,"AtlasType":1},{"ID":32,"AtlasID":"2306","AtlasTitle":"ROSI写真No.2053 衬衣黑丝门上高难度一字马","AtlasImg":"http://uuu.shafa5.com/ros/2306/2053.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/21/2306.htm","AtlasCost":20,"AtlasType":1},{"ID":33,"AtlasID":"2305","AtlasTitle":"ROSI写真No.2052 连体开档肉丝T裤翘臀","AtlasImg":"http://uuu.shafa5.com/ros/2305/2052.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/20/2305.htm","AtlasCost":20,"AtlasType":1},{"ID":34,"AtlasID":"2304","AtlasTitle":"ROSI写真No.2051 粉红内衣床上翘腿写真","AtlasImg":"http://uuu.shafa5.com/ros/2304/2051.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/19/2304.htm","AtlasCost":20,"AtlasType":1},{"ID":35,"AtlasID":"2303","AtlasTitle":"ROSI写真No.2050 黑色性感超短连衣裙肉丝美臀","AtlasImg":"http://uuu.shafa5.com/ros/2303/2050.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/18/2303.htm","AtlasCost":20,"AtlasType":1},{"ID":36,"AtlasID":"2302","AtlasTitle":"ROSI写真No.2049 沙发上绿色文胸黑丝美眉","AtlasImg":"http://uuu.shafa5.com/ros/2302/2049.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/17/2302.htm","AtlasCost":20,"AtlasType":1},{"ID":37,"AtlasID":"2301","AtlasTitle":"ROSI写真No.2048 害羞妹子运动装美腿翘臀","AtlasImg":"http://uuu.shafa5.com/ros/2301/2048.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/16/2301.htm","AtlasCost":20,"AtlasType":1},{"ID":38,"AtlasID":"2300","AtlasTitle":"ROSI写真No.2047 浴室真空肉丝美臀玉腿写真","AtlasImg":"http://uuu.shafa5.com/ros/2300/2047.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/15/2300.htm","AtlasCost":20,"AtlasType":1},{"ID":39,"AtlasID":"2299","AtlasTitle":"ROSI写真No.2046 性感粉色狐狸装T裤美腿翘臀","AtlasImg":"http://uuu.shafa5.com/ros/2299/2046.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/14/2299.htm","AtlasCost":20,"AtlasType":1},{"ID":40,"AtlasID":"2298","AtlasTitle":"ROSI写真No.2045 昏暗灯光红衣大胸黑丝撩人","AtlasImg":"http://uuu.shafa5.com/ros/2298/2045.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/13/2298.htm","AtlasCost":20,"AtlasType":1},{"ID":41,"AtlasID":"2297","AtlasTitle":"ROSI写真No.2044 衬衣真空墨绿丝袜高难度劈叉","AtlasImg":"http://uuu.shafa5.com/ros/2297/2044.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/12/2297.htm","AtlasCost":20,"AtlasType":1},{"ID":42,"AtlasID":"2296","AtlasTitle":"ROSI写真No.2043 天热火气大半裸美臀为你消火","AtlasImg":"http://uuu.shafa5.com/ros/2296/2043.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/11/2296.htm","AtlasCost":20,"AtlasType":1},{"ID":43,"AtlasID":"2295","AtlasTitle":"ROSI写真No.2042 全裸浴室泡泡浴火辣写真","AtlasImg":"http://uuu.shafa5.com/ros/2295/2042.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/10/2295.htm","AtlasCost":20,"AtlasType":1},{"ID":44,"AtlasID":"2294","AtlasTitle":"ROSI写真No.2041 衬衣真空黑丝美腿翘臀","AtlasImg":"http://uuu.shafa5.com/ros/2294/2041.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/09/2294.htm","AtlasCost":20,"AtlasType":1},{"ID":45,"AtlasID":"2293","AtlasTitle":"ROSI写真No.2040 火辣小背心内内翘臀显摆","AtlasImg":"http://uuu.shafa5.com/ros/2293/2040.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/08/2293.htm","AtlasCost":20,"AtlasType":1},{"ID":46,"AtlasID":"2292","AtlasTitle":"ROSI写真No.2039 黄色包臀短裙白色T裤翘臀","AtlasImg":"http://uuu.shafa5.com/ros/2292/2039.jpg","AtlasToUrl":"http://www.rosmm.com/rosimm/2017/07/07/2292.htm","AtlasCost":20,"AtlasType":1}]
+         */
+
+        private int type;
+        private List<PhotoData> atlasList;
+
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        public List<PhotoData> getAtlasList() {
+            return atlasList;
+        }
+
+        public void setAtlasList(List<PhotoData> atlasList) {
+            this.atlasList = atlasList;
+        }
+
+    }
+}
