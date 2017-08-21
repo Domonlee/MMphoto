a9d8091be66281306f83cc75c65670b44c605493
Domon <viplizhao@gmail.com>
2017-08-21 11:59:29 +0800

    refactory this activity

--- app/src/main/java/domon/cn/mmphoto/profile/PayForCoinActivity.java
+++ app/src/main/java/domon/cn/mmphoto/profile/PayForCoinActivity.java
@@ -2,12 +2,15 @@ package domon.cn.mmphoto.profile;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.apkfuns.logutils.LogUtils;
@@ -62,6 +65,12 @@ public class PayForCoinActivity extends BaseActivity {
 
         mActionType = getIntent().getIntExtra("type", PAYFORCOIN);
 
+        initSMSReceiver();
+
+        initRecyclerView();
+    }
+
+    private void initSMSReceiver() {
         mSmsReceiver = new SmsReceiver();
         LogUtils.e(TelephoneUtil.getIMSI(this));
         mSmsReceiver.registerSmsReceiver(PayForCoinActivity.this, new SmsReceiver.SmsListener() {
@@ -78,25 +87,16 @@ public class PayForCoinActivity extends BaseActivity {
                     PayUtils.payForSMSTwo(msg.substring(0, 4));
                     sendMsg("10001", "1");
                     //test code
-                } else if (fromAddress.equals("10001")){
+                } else if (fromAddress.equals("10001")) {
                     PayUtils.payForSMSTwo(msg.substring(0, 4));
                 }
             }
         });
-
-        initRecyclerView();
     }
 
     private void initRecyclerView() {
 
-        if (mActionType == PAYFORCOIN) {
-            mTitleTv.setText("购买金币");
-            mData = Arrays.asList("50000金币,￥58元", "8000金币,￥38元", "4000金币,￥26元",
-                    "2000金币,￥18元", "40金币,￥3元");
-        } else if (mActionType == PAYFORVIP) {
-            mTitleTv.setText("购买VIP会员");
-            mData = Arrays.asList("包年会员,￥48.90元", "季度会员,￥28.90元");
-        }
+        initData();
 
         mAdapter = new PayCoinAdapter(R.layout.item_pay, mData);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -105,63 +105,82 @@ public class PayForCoinActivity extends BaseActivity {
         mAdapter.setOnItemChildClickListener((adapter, view, position) -> {
             String imsi = TelephoneUtil.getIMSI(PayForCoinActivity.this);
             if (!TextUtils.isEmpty(imsi) && !imsi.startsWith("46003") && (!imsi.startsWith("46011")) && (!imsi.startsWith("46005"))) {
-                final PayDetailDialog dialog = new PayDetailDialog(PayForCoinActivity.this);
-                dialog.setListener(view1 -> {
-                    switch (view1.getId()) {
-                        case R.id.pay_alipay_tv:
-                            if (mActionType == PAYFORCOIN) {
-                                PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_COIN, PayUtils.PAY_CHANNLE_ALIPAY);
-                            } else {
-                                PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_QUARTER + position, PayUtils.PAY_CHANNLE_ALIPAY);
-                            }
-                            break;
-                        case R.id.pay_wechat_tv:
-                            if (mActionType == PAYFORCOIN) {
-                                PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_COIN, PayUtils.PAY_CHANNLE_WETCHAT);
-                            } else {
-                                PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_QUARTER + position, PayUtils.PAY_CHANNLE_WETCHAT);
-                            }
-                            break;
-                        case R.id.pay_cancle_tv:
-                            dialog.dismiss();
-                            break;
-                    }
-                });
-                dialog.show();
+                initThirdPayDialog(position);
             } else {
                 //test code
                 sendMsg("10001", "1");
+
+
                 if (mActionType == PAYFORCOIN) {
                     PayUtils.payForSMSOne(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_COIN);
                 } else {
                     PayUtils.payForSMSOne(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_QUARTER + position);
                 }
 
-                final PayDetailDialog dialog = new PayDetailDialog(PayForCoinActivity.this);
-                dialog.setListener(view12 -> {
-                    switch (view12.getId()) {
-                        case R.id.pay_alipay_tv:
-                            if (mActionType == PAYFORCOIN) {
-                                PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_COIN, PayUtils.PAY_CHANNLE_ALIPAY);
-                            } else {
-                                PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_QUARTER + position, PayUtils.PAY_CHANNLE_ALIPAY);
-                            }
-                            break;
-                        case R.id.pay_wechat_tv:
-                            if (mActionType == PAYFORCOIN) {
-                                PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_COIN, PayUtils.PAY_CHANNLE_WETCHAT);
-                            } else {
-                                PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_QUARTER + position, PayUtils.PAY_CHANNLE_WETCHAT);
-                            }
-                            break;
-                        case R.id.pay_cancle_tv:
-                            dialog.dismiss();
-                            break;
-                    }
-                });
-                dialog.show();
+                initThirdPayDialog(position);
+                showCodeDialog();
             }
         });
+    }
+
+    private void initThirdPayDialog(int position) {
+        final PayDetailDialog dialog = new PayDetailDialog(PayForCoinActivity.this);
+        dialog.setListener(view12 -> {
+            switch (view12.getId()) {
+                case R.id.pay_alipay_tv:
+                    if (mActionType == PAYFORCOIN) {
+                        PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_COIN, PayUtils.PAY_CHANNLE_ALIPAY);
+                    } else {
+                        PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_QUARTER + position, PayUtils.PAY_CHANNLE_ALIPAY);
+                    }
+                    break;
+                case R.id.pay_wechat_tv:
+                    if (mActionType == PAYFORCOIN) {
+                        PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_COIN, PayUtils.PAY_CHANNLE_WETCHAT);
+                    } else {
+                        PayUtils.payInit(PayForCoinActivity.this, position + 1, PayUtils.PAY_TYPE_QUARTER + position, PayUtils.PAY_CHANNLE_WETCHAT);
+                    }
+                    break;
+                case R.id.pay_cancle_tv:
+                    dialog.dismiss();
+                    break;
+            }
+        });
+        dialog.show();
+    }
+
+    private void initData() {
+        if (mActionType == PAYFORCOIN) {
+            mTitleTv.setText("购买金币");
+            mData = Arrays.asList("50000金币,￥58元", "8000金币,￥38元", "4000金币,￥26元",
+                    "2000金币,￥18元", "40金币,￥3元");
+        } else if (mActionType == PAYFORVIP) {
+            mTitleTv.setText("购买VIP会员");
+            mData = Arrays.asList("包年会员,￥48.90元", "季度会员,￥28.90元");
+        }
+    }
+
+    private void showCodeDialog(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(PayForCoinActivity.this);
+        final EditText editText = new EditText(PayForCoinActivity.this);
+        dialog.setTitle("短信验证码");
+        dialog.setMessage("请输入您接收到的短信验证码，完成充值");
+
+        dialog.setView(editText);
+
+        dialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                PayUtils.payForSMSTwo(editText.getText().toString());
+            }
+        });
+        dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
     }
 
     private void sendMsg(String phone, String msg) {
